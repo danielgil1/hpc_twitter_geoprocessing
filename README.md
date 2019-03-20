754ff117006e5513ea33df59e6b5241cf5d5a4f2
danielfelipegil <dfgilto@gmail.com>
2019-03-20 11:43:42 +1100

    Load in dataframe and filtered by melbourne

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hpc_twitter_geoprocessing
 HPC processing
+
+Login:
+ssh your-unimelb-username@spartan.hpc.unimelb.edu.au
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from pandas.io.json import json_normalize
+import json
+from pprint import pprint
 
 def preprocess():
-    df=pd.read_json("data/tinyTwitter.json")
-    print(df.head())
+    filename="data/tinyTwitter.json"
+    #df=pd.read_json(filename)
+    #print(df.head())
+
+    with open(filename) as f:
+        data = json.load(f)
+        #pprint(data)
+
+    
+    df3=pd.DataFrame.from_dict(json_normalize(data['rows']), orient='columns')
+    print(df3.columns)
+    df4=df3[df3['value.properties.location']=='melbourne'][['doc._id','doc.coordinates.coordinates','doc.text']]
+    print(df4.head())
+    print(df4.shape)
 
 if __name__ == "__main__":
     preprocess()
